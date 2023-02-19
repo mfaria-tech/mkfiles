a87e234bc899155e13f087f1ec432f2218fff493
Marcus Faria <mfaria150.github@gmail.com>
2023-02-19 10:31:55 -0300

    feat(errs): Started error-handling file
    Start struct type errs.
    Set directives to struct errs.
    Define const to write file.

---
+++ errs.h
@@ -0,0 +1,24 @@
+/**
+ * TITLE: errs.h
+ * NAME: Marcus Faria
+ * CREATED AT: 19.02.2023
+ * UPDATED AT: 19.02.2023
+ *
+ * DESCRIPTION:
+**/
+
+#include <stdlib.h>
+
+#define FILENAME "logs_mkfiles"
+
+typedef struct
+{
+    int code;
+    char *log;
+    char *msg;
+    char *logdate;
+}
+errs;
+
+int get_err(int code);
+
