896492ddd7cf06bd00addddc20d91c59033c9bd4
Marcus Faria <mfaria150.github@gmail.com>
2023-02-19 10:46:29 -0300

    feat(mkfiles): Add lib to get options

--- mkfiles.c
+++ mkfiles.c
@@ -7,5 +7,19 @@
  * DESCRIPTION:
 **/
 
+#include <getopt.h>
 #include <stdio.h>
+
+int main (int argc,char *argv[])
+{
+    char *opts = "i";
+    
+    char opt = getopt(argc, argv, opts); 
+    if (opt == '?')
+    {
+        return 1;
+    }
+
+    return 0;
+}
 
