c1f9c58a1808739754e6ddfb482b595c567d35ab
Marcus Faria <mfaria150.github@gmail.com>
2023-02-21 22:56:57 -0300

    feat(errs): Set function to Read File Logs
    Added function to read file log if exist, and return content value

--- errs.c
+++ errs.c
@@ -48,11 +48,51 @@ void set_log(errs *err, char *log, char *msg)
 
 void save_log(errs *err)
 {
+    char *logs = read_file();
+
     FILE *logfile = fopen(FILENAME, "w");
     if (logfile == NULL)
     {
         printf("\e[0;30mCould not open %s.\e[0m\n", FILENAME);
         return;
     }
+    if (logs != NULL)
+    {
+        for (int i = 0; logs[i] != '\0'; i++)
+        {
+            fputc(logs[i], logfile);
+        }
+    }
+
+    fclose(logfile);
+}
+
+// read file with exist
+char *read_file(void)
+{
+    FILE *curfile = fopen(FILENAME, "r");
+    if (curfile == NULL)
+    {
+        return NULL;
+    }
+
+    long int cletters = 1; // 1 == '\0'
+    fseek(curfile, 0, SEEK_SET);
+    while (fgetc(curfile) != EOF)
+    {
+        cletters++;
+    }
+
+    char *logs = malloc(cletters * sizeof(char));
+    fseek(curfile, 0, SEEK_SET);
+    for (int i = 0, ch;
+         ( ch = fgetc(curfile) ) != EOF;
+         i++)
+    {
+        logs[i] = ch;
+    }
+    logs[cletters - 1] = '\0';
+    fclose(curfile);
+    return logs;
 }
 
